801e5412b4e657b25416485d054ad8032de69d5e
Tom Victor <vjtomvictor@icloud.com>
2017-04-06 20:57:38 +0530

    code reverted due to error

--- Tour of Heroes/src/app/app.component.js
+++ Tour of Heroes/src/app/app.component.js
@@ -15,10 +15,8 @@ exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = {
-            id: 1,
-            name: 'Windstorm'
-        };
+        this.hero = 'Windstorm';
+        this.derod = HEROES;
     }
     return AppComponent;
 }());
@@ -29,4 +27,16 @@ AppComponent = __decorate([
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 //# sourceMappingURL=app.component.js.map
