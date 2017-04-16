b7fc0a861c30d5bb08ebc3b19012f8eb7215e1a4
Tom Victor <vjtomvictor@icloud.com>
2017-04-16 14:07:08 +0530

    Angular service methods added

--- ng-routing/src/app/service/characters.service.ts
+++ ng-routing/src/app/service/characters.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Character } from '../classes/character' ;
+import { CHARACTERS } from '../classes/data-characters'
 
 @Injectable()
 export class CharactersService {
 
   constructor() { }
+  getCharacter():Promise<Character[]> {
+    return Promise.resolve(CHARACTERS) ;
+  }
 
 }
